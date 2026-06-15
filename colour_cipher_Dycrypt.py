a6c1ef33d8e56fcb47c71370bce3a503e6a86b41
CreatedBySarika <sahsarika2005@gmail.com>
2026-06-15 19:54:05 +0100

    can decrypt

--- colour_cipher_Dycrypt.py
+++ colour_cipher_Dycrypt.py
@@ -7,22 +7,36 @@ Alphabet = {
     "RGYBCP" : "F" ,
     "GRYBCP" : "G" ,
     "GYRBCP" : "H",
-    "GYBRCP" :"I",
+    "GYBRCP" : "I",
     "GYBCRP" : "J",
     "GYBCPR" : "K",
-    "L" : "YGBCPR",
-    "M" : "YBGCPR",
-    "N" : "YBCGPR",
-    "O" : "YBCPGR",
-    "P" : "YBCPRG",
-    "Q" : "BYCPRG",
-    "R" : "BCYPRG",
-    "S" : "BCPYRG",
-    "T" : "BCPRYG",
-    "U" : "BCPRGY",
-    "V" : "CBPRGY",
-    "W" : "CPBRGY",
-    "X" : "CPRBGY",
-    "Y" : "CPRGBY", 
-    "Z" : "CPRGYB"
+    "YGBCPR" : "L",
+    "YBGCPR" : "M",
+    "YBCGPR" : "N",
+    "YBCPGR" : "O",
+    "YBCPRG" : "P",
+    "BYCPRG" : "Q",
+    "BCYPRG" : "R",
+    "BCPYRG" : "S",
+    "BCPRYG" : "T",
+    "BCPRGY" : "U",
+    "CBPRGY" : "V",
+    "CPBRGY" : "W",
+    "CPRBGY" : "X",
+    "CPRGBY" : "Y", 
+    "CPRGYB" : "Z"
     }
+print("There are 6 colour codes used R-Red,P-Pink,C-Cyan,B-Blue,G-Green,Y-Yellow")
+print("*************************************************************************")
+decrypt = []
+code_input = input("Enter the code seperated by comma in all CAPS here  : ")
+codes = code_input.split(",")
+#print(codes)
+
+for i in codes:
+    if i in Alphabet:
+        decrypt.append(Alphabet.get(i))
+    else:
+        print("Incorrect code")
+
+print ("".join(decrypt))
